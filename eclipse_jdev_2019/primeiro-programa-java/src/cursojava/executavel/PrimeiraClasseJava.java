608c2a34d20e48d08be3bce75ca5905736131757
charlescript <rocha_charles@outlook.com>
2025-12-27 15:54:45 -0300

    Criando primeiros programas em java na versão 8 e 25

---
+++ eclipse_jdev_2019/primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,10 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é um método auto executavel em Java*/
+	public static void main(String[] args) {
+		//Escreve texto no console
+		System.out.println("Primeiro programa em Java");
+	}
+}
--- primeiro_programa/src/CursoJava.java
+++
@@ -1,7 +0,0 @@
-class CursoJava {
-
-  public static void main(String[] args) {
-    System.out.println("Primeiro programa em Java");
-  }	
-	
-}
