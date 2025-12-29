4c1a07cc59a6737a5fb74f88481c626307e015d7
charlescript <rocha_charles@outlook.com>
2025-12-29 00:24:14 -0300

    Comitando através do Eclipse 2019

---
+++ eclipse_2025/projetojava1/src/projetojava1/executavel/resteRepository.java
@@ -0,0 +1,9 @@
+package projetojava1.executavel;
+
+public class resteRepository {
+	
+	public static void main(String[] args) {
+		System.out.println("Subindo novo projeto a partir de repositorio local -> remoto através de ECLIPSE 2025");
+		System.out.println("Conectando repositório local -> remoto via eclipse 2025");
+	}
+}
