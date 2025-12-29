79adedea761f2ff432374ba84e10513f1f9eaa16
charlescript <rocha_charles@outlook.com>
2025-12-29 02:33:45 -0300

    Comitando para configurar a IDE eclipse 2025 no repositório remoto via token granularizado

--- eclipse_2025/projetojava1/src/projetojava1/executavel/testeRepository.java
+++ eclipse_2025/projetojava1/src/projetojava1/executavel/testeRepository.java
@@ -5,5 +5,6 @@ public class testeRepository {
 	public static void main(String[] args) {
 		System.out.println("Subindo novo projeto a partir de repositorio local -> remoto através de ECLIPSE 2025");
 		System.out.println("Conectando repositório local -> remoto via eclipse 2025");
+		System.out.println("Configurando a IDE eclipse 2025 a repositório remoto com tokem granularizado");
 	}
 }
