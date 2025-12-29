702f23a08456a5ec669ab5b6857ee968d3d906eb
charlescript <rocha_charles@outlook.com>
2025-12-29 02:03:00 -0300

    Ajuste de identação

--- eclipse_jdev_2019/projetojava1/src/projetojava1/executavel/testeRepository.java
+++ eclipse_jdev_2019/projetojava1/src/projetojava1/executavel/testeRepository.java
@@ -3,8 +3,10 @@ package projetojava1.executavel;
 public class testeRepository {
 	
 	public static void main(String[] args) {		
+		
 		System.out.println("Subindo novo projeto a partir do eclipse 2019");
 		System.out.println("Conectando repositório local -> remoto a partir do Eclipse 2019");
+		
 	}
 	
 }
