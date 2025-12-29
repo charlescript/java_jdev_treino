1f0a1c6d0da504cfd900734ec623be1c8e45eecb
charlescript <rocha_charles@outlook.com>
2025-12-29 04:34:57 -0300

    Classe 'Pessoa' criada para commit

---
+++ eclipse_jdev_2019/projetojava1/src/curso/java/charles/Pessoa.java
@@ -0,0 +1,31 @@
+package curso.java.charles;
+
+public class Pessoa {
+	
+	private String nome;
+	private String sobrenome;
+	
+	// ---------------------------------------
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	// ---------------------------------------
+	
+	public String getSobrenome() {
+		return nome;
+	}
+	
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	// ---------------------------------------
+
+	
+}
