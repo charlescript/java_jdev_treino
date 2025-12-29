05b2c5ef292dc88dabed749a07a024b425590737
charlescript <rocha_charles@outlook.com>
2025-12-29 17:16:27 -0300

    Adicionando dataNascimento2 na classe Pessoa em eclipse 2025

--- eclipse_2025/projetojava1/src/curso/java/charles/Pessoa.java
+++ eclipse_2025/projetojava1/src/curso/java/charles/Pessoa.java
@@ -9,6 +9,7 @@ public class Pessoa {
 	private int idade;
 	private Date dataNascimento;
 	
+	private Date datanascimento2;
 	//--------------------------------
 	public String getNome() {
 		return nome;
