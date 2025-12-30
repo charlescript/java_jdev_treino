f051475247aaa18b98dd1d55d399a008accebaeb
charlescript <rocha_charles@outlook.com>
2025-12-30 16:20:15 -0300

    Atualização

--- eclipse_jdev_2019/projetojava1/src/projetojava1/executavel/testeRepository.java
+++ eclipse_jdev_2019/projetojava1/src/projetojava1/executavel/testeRepository.java
@@ -5,7 +5,6 @@ public class testeRepository {
 	public static void main(String[] args) {		
 		System.out.println("Subindo novo projeto a partir do eclipse 2019");
 		System.out.println("Conectando repositório local -> remoto a partir do Eclipse 2019");
-	
 	}
 	
 }
