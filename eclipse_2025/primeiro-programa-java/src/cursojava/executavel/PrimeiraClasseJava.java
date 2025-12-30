8e2d0184d13b2c5d80a16fd10373e1c11363c6c5
charlescript <rocha_charles@outlook.com>
2025-12-30 18:40:39 -0300

    Utilizando uma branch paralela 'tarefa_1_branch_1'

--- eclipse_2025/primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ eclipse_2025/primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -6,6 +6,8 @@ public class PrimeiraClasseJava {
 	public static void main( String[] args ) {
 		System.out.println("Meu primeiro programa em Java do curso -> Formação Java Web");
 		System.out.println("Java é orientado a objeto");
+	
+		System.out.println("Utilizando uma branch paralela 'tarefa_1_branch_1'");
 	}
 	
 }
