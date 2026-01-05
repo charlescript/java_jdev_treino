bebf1dcfcafa5b8b7643000792faf2c71ee5b154
charlescript <rocha_charles@outlook.com>
2026-01-05 07:15:56 -0300

    Trabalhando com String e concatenação de dados

---
+++ tiposvariaveis/src/tiposvariaveis/ConcatenacaoVariaveis.java
@@ -0,0 +1,20 @@
+package tiposvariaveis;
+
+public class ConcatenacaoVariaveis {
+	
+	public static void main(String[] args) {
+		
+		/** Concatenação = Unir ou juntar dados de diferentes tipos
+		 *  e lugares de forma que fique legível e informacional */
+		String texto = "Informativo sobre questões envolvendo a geopolitica global no cenário de 2026";
+		String cpf = "123.456.789-10";
+		String telefone = "(11) 99183-1548";
+		String endereco = "Rua Almirante Danton";
+		
+		System.out.println("Notícia: " + texto);
+		System.out.println("CPF:" + cpf);
+		System.out.println("Telefone: "+ telefone);
+		System.out.println("Endereco: " + endereco);
+		
+	}
+}
---
+++ tiposvariaveis/src/tiposvariaveis/TipoStringDados.java
@@ -0,0 +1,18 @@
+package tiposvariaveis;
+
+public class TipoStringDados {
+	
+	public static void main(String[] args) {
+		
+		/*String no Java sempre será um texto de tamanho N(caracter)*/
+		String texto = "Informativo sobre questões envolvendo a geopolitica global no cenário de 2026";
+		String cpf = "123.456.789-10";
+		String telefone = "(11) 99183-1548";
+		String endereco = "Rua Almirante Danton";
+		
+		System.out.println(texto);
+		System.out.println("CPF:" + cpf);
+		System.out.println("Telefone: "+ telefone);
+		System.out.println("Endereco: " + endereco);
+	}
+}
