84f3e42f50eecaa556a7c034834c76c3b112b0b9
charlescript <rocha_charles@outlook.com>
2026-01-05 06:47:58 -0300

    Adicionando itens ao git ignore para evitar erros e conteudo desnecessários, -> Trabalhando com tipos Double

---
+++ tiposvariaveis/src/tiposvariaveis/OperacoesNumerosInteiros.java
@@ -0,0 +1,15 @@
+package tiposvariaveis;
+
+public class OperacoesNumerosInteiros {
+	
+	public static void main(String[] args) {
+		
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 50;
+		int nota4 = 88;
+		float resultado = nota1 + nota2 + nota3 + nota4;
+		
+		System.err.println("Média das notas: " + (resultado) / 4);
+	}
+}
---
+++ tiposvariaveis/src/tiposvariaveis/TipoDoubleDados.java
@@ -0,0 +1,15 @@
+package tiposvariaveis;
+
+public class TipoDoubleDados {
+	
+	public static void main(String[] args) {
+		
+		double nota1 = 90;
+		double nota2 = 70;
+		double nota3 = 50;
+		double nota4 = 88;
+		double mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		System.out.println("Média das notas: " + mediaFinal);
+	}
+}
