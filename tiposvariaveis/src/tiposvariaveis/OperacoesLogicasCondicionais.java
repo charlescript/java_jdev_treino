5122bcf7b1f0defdb128cee8781f4b9cb06aeb56
charlescript <rocha_charles@outlook.com>
2026-01-22 21:13:47 -0300

    Operações lógicas -> ternários

--- tiposvariaveis/src/tiposvariaveis/OperacoesLogicasCondicionais.java
+++ tiposvariaveis/src/tiposvariaveis/OperacoesLogicasCondicionais.java
@@ -3,15 +3,29 @@ package tiposvariaveis;
 public class OperacoesLogicasCondicionais {
 	public static void main(String[] args) {
 		
-		int mediaAluno = 49;
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 95;
+		int nota4 = 68;		
 		
+		int mediaAluno = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		/** Condições lógicas com IF e Else*/
 		if(mediaAluno >= 70 ) {
 			System.out.println("Parábens você esta aprovado");
-		} else if(mediaAluno >= 50) {
+		} 
+		 else if(mediaAluno >= 50) {
 			System.out.println("Aluno de recuperação.");
 		} else {
 			System.out.println("Aluno reprovado");
 		}
+		
+		
+		/** Operadores ternários são para micro validações */
+		System.out.println("\n---------- Ternários ----------");
+		String saidaResultado;
+		saidaResultado = mediaAluno >= 70 ? "Aprovado" : mediaAluno >= 50 ? "Recuperação" : "Reprovado";
+		System.out.println("Aluno: " + saidaResultado);
 		
 	}
 }
