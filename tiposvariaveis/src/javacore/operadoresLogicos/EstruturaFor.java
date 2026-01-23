720afab1a792ebc35a0bb038a88ed9e07ea88c68
charlescript <rocha_charles@outlook.com>
2026-01-23 03:29:22 -0300

    Repetição com FOR

---
+++ tiposvariaveis/src/javacore/operadoresLogicos/EstruturaFor.java
@@ -0,0 +1,19 @@
+package javacore.operadoresLogicos;
+
+public class EstruturaFor {
+	public static void main(String[] args) {
+		// Estrutura de repetição FOR
+		
+		for(int numero = 0; numero <= 10; numero++) {
+			System.out.println("Posição: " + numero + "º");
+		}
+		
+		//---------------------------------
+		System.out.println("\n------------------");
+		
+		for(int i = 10; i >= 0; i-- ) {
+			System.out.println("Posição: " + i+ "º" );
+		}
+		
+	} 
+}
