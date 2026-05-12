52362cb8a7c38fef5e629c64c97be624d3b8b071
charlescript <rocha_charles@outlook.com>
2026-05-12 06:53:11 -0300

    Testando acoplamento via IDE Eclipse

---
+++ primeiro_programa_java/src/javaTeste/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package javaTeste.executavel;
+
+public class PrimeiraClasseJava {
+
+    // main é o método auto executavel
+    public static void main(String[] args){
+        System.out.println("Não é o meu primeiro programa em Java");
+        System.out.println("Java é orientado a objeto");
+    }
+}
+ 
