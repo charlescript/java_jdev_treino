edf955985e2146df8b08d3bad5b2f7f6de32bd55
charlescript <rocha_charles@outlook.com>
2026-05-12 07:32:00 -0300

    Teste de sincronização via IDE Eclipse

--- primeiro_programa_java/src/javaTeste/executavel/PrimeiraClasseJava.java
+++ primeiro_programa_java/src/javaTeste/executavel/PrimeiraClasseJava.java
@@ -6,7 +6,7 @@ public class PrimeiraClasseJava {
     public static void main(String[] args){
         System.out.println("Não é o meu primeiro programa em Java");
         System.out.println("Java é orientado a objeto");
-        System.out.println("Sincronizando IDE eclipse ao repositório remote");
+        System.out.println("Sincronizando IDE eclipse ao repositório remoto auth");
     }
 }
  
