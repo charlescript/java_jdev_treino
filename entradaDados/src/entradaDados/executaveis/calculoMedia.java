e8344a4ae2c28f251a5c75c87cf5b74c70e494df
charlescript <rocha_charles@outlook.com>
2026-05-13 05:00:03 -0300

    Utilizando for para coletar notas e apresentar media via interface gráfica

--- entradaDados/src/entradaDados/executaveis/calculoMedia.java
+++ entradaDados/src/entradaDados/executaveis/calculoMedia.java
@@ -1,5 +1,31 @@
 package entradaDados.executaveis;
 
-public class calculoMedia {
+import javax.swing.JOptionPane;
 
-}
+public class calculoMedia {
+	
+	public static void main(String[] args) {
+		
+		double total = 0;
+		double media = 0;
+		int quantidade = Integer.parseInt(JOptionPane.showInputDialog("Quantas notas serão apuradas ?"));
+		
+		for(int i = 1; i <= quantidade; i++) {
+			double nota = Double.parseDouble(JOptionPane.showInputDialog("Digite a " + i +"º nota: "));			
+			total += nota;
+		}
+		
+		media = (total / quantidade);
+		
+		if (media >= 50) {
+			if (media >= 70) {
+				JOptionPane.showMessageDialog(null, "Aluno está aprovado com média de: " + media);
+			} else {
+				JOptionPane.showMessageDialog(null, "Aluno está de recuperação, com média: " + media);
+			}
+		} else {
+			JOptionPane.showMessageDialog(null, "Aluno está reprovado com média de: " + media );
+		}
+		
+	} // Fim método main
+} 
