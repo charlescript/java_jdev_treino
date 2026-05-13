b231e64b26d77dab05ca26b96e3cdb7961067e8a
charlescript <rocha_charles@outlook.com>
2026-05-13 00:09:05 -0300

    Buscando resto de divisão com módulo %

---
+++ EstruturaRepeticao/src/estruturaRepeticao/moduloJava.java
@@ -0,0 +1,19 @@
+package estruturaRepeticao;
+
+public class moduloJava {
+	
+	public static void main(String[] args) {
+		
+		/* Módulo: Resto da divisão % */
+		
+		double carro = 9;
+		double pessoa = 2;
+		
+		
+		double resto = carro % pessoa;
+		
+		System.out.println("Sobraram exatamente: " + resto + " carros ");
+		
+	}
+
+}
