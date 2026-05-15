369872493e3cd29d95d21304d02fe611f11a51d3
charlescript <rocha_charles@outlook.com>
2026-05-15 19:54:54 -0300

    Atributos privados, com acessos publicos getters, setters

--- programacao_poo/src/cursojava/classes/Aluno.java
+++ programacao_poo/src/cursojava/classes/Aluno.java
@@ -4,27 +4,132 @@ import javax.swing.JOptionPane;
 
 public class Aluno {
 	
-	public String nome;
-	public int idade;
-	public String dataNascimento;
-	String registroGeral;
-	String numeroCpf;
-	String nomeMae;
-	String nomePai;
-	String dataMatricula;
-	String nomeEscola;
-	String serieMatriculado;
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
 	
-	public Aluno() {}
+	//public Aluno() {}
 	
 	public Aluno(String nome, int idade, String dataNascimento) {
-		this.nome = nome;
-		this.idade = idade;
-		this.dataNascimento = dataNascimento;
+		this.setNome(nome);
+		this.setIdade(idade);
+		this.setDataNascimento(dataNascimento);
 	}
 	
+	/*Métodos Getters e Setters para acesso dos atributos privados*/
+	/*Set adicionado dados no atributo da instância em tempo de execução*/
+	/*Get recuperando dados do atributo da instância em tempo de execução*/
+	
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	//-//////////////////////////////////
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	public int getIdade() {
+		return this.idade;
+	}
+	//-//////////////////////////////////
+	
+	public void setDataNascimento(String data) {
+		this.dataNascimento = data;
+	}
+	
+	public String getDataNascimento() {
+		return this.dataNascimento;
+	}
+	//-//////////////////////////////////
+	
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+	
+	//-//////////////////////////////////
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+	
+	//-//////////////////////////////////
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+	
+	//-//////////////////////////////////
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	//-//////////////////////////////////
+	
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+	
+	//-//////////////////////////////////
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+	
+	//-//////////////////////////////////
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+	
+	//-//////////////////////////////////
 	
 	public void retornaDados(){
-		JOptionPane.showMessageDialog(null,"Nome: "+ this.nome + "\nIdade: "+ this.idade + "\nNascimento: "+this.dataNascimento);
+		JOptionPane.showMessageDialog(null,"Nome: "+ this.getNome() + 
+				"\nIdade: "+ this.getIdade() + 
+				"\nNascimento: "+ this.getDataNascimento() +
+				"\nRG: " + this.getRegistroGeral() + 
+				"\nCPF: " + this.getNumeroCpf());
 	}
 }
--- programacao_poo/src/cursojava/executavel/PrimeiraClasseJava.java
+++ programacao_poo/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -9,12 +9,20 @@ public class PrimeiraClasseJava {
 		
 		Aluno aluno1 = new Aluno("Alex", 35, "10/10/1991");
 		Aluno aluno2 = new Aluno("João", 39, "10/10/1987");
-		Aluno aluno3 = new Aluno();
 		
 		aluno1.retornaDados();
 		
-		aluno2.nome = "João 2";
-		aluno2.idade = 40;
+		aluno2.setNome("João 2");
+		aluno2.setIdade(40);
+		aluno2.setDataNascimento("10/11/1987");
+		aluno2.setRegistroGeral("41.325.147-6");
+		aluno2.setNumeroCpf("330.682.554-47");
+		aluno2.setNomeMae("Maria");
+		aluno2.setNomePai("Edgar");
+		aluno2.setDataMatricula("10/03/2026");
+		aluno2.setNomeEscola("FATEC");
+		aluno2.setSerieMatriculado("2º Semestre");
+		
 		aluno2.retornaDados();
 	}
 
